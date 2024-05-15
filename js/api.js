ab2550cf5ebf6666ca324735dc338ba5b006a2f4
Andrew Horn de Borba <Andrewhorndeborba@gmail.com>
2024-05-15 20:56:48 -0300

    conexão com a API para as opções, links estão quase 100%

--- js/api.js
+++ js/api.js
@@ -1,30 +1,51 @@
-//listar as moedas no select
-const URLlistaMoedas = 'https://economia.awesomeapi.com.br/json/available/uniq';
-var listaMoedas;
+new Vue({
+    el: '#app',
+    data: {
+        URLlistaMoedas: 'https://economia.awesomeapi.com.br/json/available/uniq',
+        listaMoedas: [
+            { id: 1, text: 'Primeiro item' },
+            { id: 2, text: 'Segundo item' },
+            { id: 3, text: 'Terceiro item' },
+            { id: 4, text: 'Quarto item' }
+        ],
+        opcoes : [],
+        ultimoAtivo : false,
+    },
+    created() {
+        fetch(this.URLlistaMoedas, {method: "GET",})
+        .then(resp => resp.json())
+        .then(data => {
+            this.opcoes = Object.keys(data).map(key => ({ key: key, text: data[key] }));
+        })
+        .catch(error => console.error('Erro ao buscar dados:', error));
+    },
+    methods: {
+        pegaFiltros() {
+            const de = document.getElementById('code').value;
+            const para = document.getElementById('codein').value;
+            const ultimo = document.getElementById('last').checked;
 
-fetch(URLlistaMoedas, {
-    method : "GET",
-})
-.then( (resp) => resp.json() )
-.then( (data) => {
-    listaMoedas = Object.keys(data);
-    for (var index = 0; index < listaMoedas.length; index++) {
+            let url;
+            if (ultimo) {
+                // se last estiver ativo
+                url = `https://economia.awesomeapi.com.br/json/last/${de}-${para}`;
+            } else {
+                const periodo = document.getElementById('start_date').value;
+                const ate = document.getElementById('end_date').value;
+
+                url = `https://economia.awesomeapi.com.br/json/daily/${de}-${para}?start_date=${periodo}&end_date=${ate}`;
+            }
+
+            this.pegaDados(url);
+        },
+        pegaDados(url) {
+            fetch(url, {method: "GET",})
+            .then(resp => resp.json())
+            .then(data => {
+                console.log(Object.keys(data).map(key => ({ key: key, text: data[key] })));
+            })
+            .catch(error => console.error('Erro ao buscar dados:', error));
+    
+        }
     }
-})
-
-var de = document.getElementById('code');
-var para = document.getElementById('codein');
-var periodo = document.getElementById('start_date');
-var ate = document.getElementById('end_date');
-var ultimo = document.getElementById('last');
-
-
-//pegar a função do sor pra depois do submit da consulta executar isso
-function confereLast() {
-    if (ultimo.checked == true) {
-        //se last estiver ativo
-        console.log('oiii');
-    } else{
-        //testa se o periodo e o até está ok
-    }
-}
+});
